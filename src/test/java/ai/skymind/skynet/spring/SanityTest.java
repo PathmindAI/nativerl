52ea5532636e20e9ab4653f4eabfa68e58b79ab7
Eduardo Gonzalez <me@wm-eddie.com>
2019-04-24 17:38:12 +0900

    Add Simple JUnit test.

---
+++ src/test/java/ai/skymind/skynet/spring/SanityTest.java
@@ -0,0 +1,13 @@
+package ai.skymind.skynet.spring;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SanityTest {
+    @Test
+    public void testJUnit() {
+
+        assertTrue(true);
+    }
+}
