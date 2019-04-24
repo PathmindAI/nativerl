d705a9e2659088840ed32806a9a4e3ae9002fb12
Eduardo Gonzalez <me@wm-eddie.com>
2019-04-24 18:14:21 +0900

    Switch to JDK 11 and add run configurations for testing.

--- src/main/java/ai/skymind/skynet/spring/MainView.java
+++ src/main/java/ai/skymind/skynet/spring/MainView.java
@@ -13,7 +13,7 @@ import com.vaadin.flow.server.PWA;
 public class MainView extends VerticalLayout {
 
     public MainView(@Autowired MessageBean bean) {
-        Button button = new Button("Click me", e -> Notification.show(bean.getMessage()));
+        var button = new Button("Click me", e -> Notification.show(bean.getMessage()));
         add(button);
     }
 
