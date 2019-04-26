de83330c0d0bcfb13b76213384b982226d5bcc23
Eduardo Gonzalez <me@wm-eddie.com>
2019-04-26 15:27:34 +0900

    Reformat code.

--- src/main/java/ai/skymind/skynet/spring/MainView.java
+++ src/main/java/ai/skymind/skynet/spring/MainView.java
@@ -13,7 +13,11 @@ import com.vaadin.flow.server.PWA;
 public class MainView extends VerticalLayout {
 
     public MainView(@Autowired MessageBean bean) {
-        Button button = new Button("Click me", e -> Notification.show(bean.getMessage()));
+        Button button = new Button("Click me", e -> {
+            Notification.show(bean.getMessage());
+        });
+
+
         add(button);
     }
 
