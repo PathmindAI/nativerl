1f0c20dc3a6e4890c38c1d880110960f9834a5c8
Paul Dubs <paul.dubs@gmail.com>
2019-07-10 14:11:25 +0200

    Add Error messages to code editors

--- webapp/src/main/kotlin/ai/skymind/skynet/spring/cloud/job/local/RLConfig.kt
+++ webapp/src/main/kotlin/ai/skymind/skynet/spring/cloud/job/local/RLConfig.kt
@@ -2,6 +2,11 @@ package ai.skymind.skynet.spring.cloud.job.local
 
 import ai.skymind.skynet.data.db.jooq.tables.records.MdpRecord
 import ai.skymind.skynet.data.db.jooq.tables.records.ModelRecord
+import ai.skymind.skynet.spring.views.CharSequenceJavaFileObject
+import java.util.*
+import javax.tools.DiagnosticCollector
+import javax.tools.JavaFileObject
+import javax.tools.ToolProvider
 
 data class RLConfig(
         val outputFileName: String,
@@ -21,8 +26,51 @@ data class RLConfig(
         else -> throw IllegalArgumentException("${model.timeUnit} is an Invalid Time Unit")
     }
 
-    fun toTrainingFile() = """
 
+    data class CompileErrors(val imports: MutableList<String> = mutableListOf(), val variables: MutableList<String> = mutableListOf(), val reset: MutableList<String> = mutableListOf(), val reward: MutableList<String> = mutableListOf(), val metrics: MutableList<String> = mutableListOf())
+    data class CodeRanges(val imports: Pair<Int?, Int?> = null to null, val variables: Pair<Int?, Int?> = null to null, val reward: Pair<Int?, Int?> = null to null, val metrics: Pair<Int?, Int?> = null to null, val reset: Pair<Int?, Int?> = null to null)
+    fun compileErrors(): CompileErrors {
+        val code = toTrainingFile()
+        val lines = code.split("\n")
+        val range = lines.mapIndexed { idx, line -> idx to line }.fold(CodeRanges()) { acc, pair ->
+            val (index, line) = pair
+            when (line) {
+                "// START CUSTOM IMPORTS 8279520310" -> acc.copy(imports = acc.imports.copy(first = index))
+                "// END CUSTOM IMPORTS 8279520310" -> acc.copy(imports = acc.imports.copy(second = index))
+
+                "// START CUSTOM VARIABLES 4998274237" -> acc.copy(variables = acc.variables.copy(first = index))
+                "// END CUSTOM VARIABLES 4998274237" -> acc.copy(variables = acc.variables.copy(second = index))
+
+                "// START CUSTOM RESET 0119724160" -> acc.copy(reset = acc.reset.copy(first = index))
+                "// END CUSTOM RESET 0119724160" -> acc.copy(reset = acc.reset.copy(second = index))
+
+                "// START CUSTOM REWARD 2934951523" -> acc.copy(reward = acc.reward.copy(first = index))
+                "// END CUSTOM REWARD 2934951523" -> acc.copy(reward = acc.reward.copy(second = index))
+
+                "// START CUSTOM METRICS 2867541152" -> acc.copy(metrics = acc.metrics.copy(first = index))
+                "// END CUSTOM METRICS 2934951523" -> acc.copy(metrics = acc.metrics.copy(second = index))
+
+                else -> acc
+            }
+        }
+
+        val diagnostics = DiagnosticCollector<JavaFileObject>()
+        val compiler = ToolProvider.getSystemJavaCompiler()
+        compiler.getTask(null, null, diagnostics, null, null, listOf(CharSequenceJavaFileObject("NewTraining", code))).call()
+
+        return diagnostics.diagnostics.fold(CompileErrors()){ acc, diag ->
+            when(diag.lineNumber){
+                in range.imports.first!!..range.imports.second!! -> acc.imports.add("${diag.kind}: Line ${diag.lineNumber - range.imports.first!! - 1}: ${diag.getMessage(Locale.ROOT)}")
+                in range.variables.first!!..range.variables.second!! -> acc.variables.add("${diag.kind}: Line ${diag.lineNumber - range.variables.first!! - 1}: ${diag.getMessage(Locale.ROOT)}")
+                in range.reset.first!!..range.reset.second!! -> acc.reset.add("${diag.kind}: Line ${diag.lineNumber - range.reset.first!! - 1}: ${diag.getMessage(Locale.ROOT)}")
+                in range.reward.first!!..range.reward.second!! -> acc.reward.add("${diag.kind}: Line ${diag.lineNumber - range.reward.first!! - 1}: ${diag.getMessage(Locale.ROOT)}")
+                in range.metrics.first!!..range.metrics.second!! -> acc.metrics.add("${diag.kind}: Line ${diag.lineNumber - range.metrics.first!! - 1}: ${diag.getMessage(Locale.ROOT)}")
+            }
+            acc
+        }
+    }
+
+    fun toTrainingFile() = """
 import com.anylogic.engine.Agent;
 import com.anylogic.engine.AgentConstants;
 import com.anylogic.engine.AgentList;
@@ -46,11 +94,15 @@ import org.deeplearning4j.rl4j.util.DataManager;
 import org.json.JSONObject;
 import org.nd4j.linalg.learning.config.AdaDelta;
 
+// START CUSTOM IMPORTS 8279520310
 ${mdp.imports ?: ""}
+// END CUSTOM IMPORTS 8279520310
 
 public class NewTraining extends ExperimentCustom {
     public class AnylogicToRLAdapter {
+// START CUSTOM VARIABLES 4998274237
       ${mdp.variables  ?: ""}
+// END CUSTOM VARIABLES 4998274237
     
       public ObservationSpace<Encodable> getObservationSpace(){
         ObservationSpace<Encodable> space = new ArrayObservationSpace<>(new int[] {${mdp.observationSpaceSize}});
@@ -63,19 +115,25 @@ public class NewTraining extends ExperimentCustom {
       }
       
       public void reset(Main agent){
+// START CUSTOM RESET 0119724160
         ${mdp.reset  ?: ""}
+// END CUSTOM RESET 0119724160
       }
       
       
       public double reward(Main agent, double[] before, double[] after) {
         double reward = 0;
+// START CUSTOM REWARD 2934951523
         ${mdp.reward  ?: ""}
+// END CUSTOM REWARD 2934951523
         return reward;
       }
       
       public double[] metrics(Main agent){
         double[] metrics = null;
+// START CUSTOM METRICS 2867541152
         ${mdp.metrics  ?: ""}
+// END CUSTOM METRICS 2934951523
         return metrics;
       }
     
--- webapp/src/main/kotlin/ai/skymind/skynet/spring/views/EditMdpView.kt
+++ webapp/src/main/kotlin/ai/skymind/skynet/spring/views/EditMdpView.kt
@@ -1,6 +1,8 @@
 package ai.skymind.skynet.spring.views
 
 import ai.skymind.skynet.data.db.jooq.tables.records.MdpRecord
+import ai.skymind.skynet.spring.cloud.job.local.Environment
+import ai.skymind.skynet.spring.cloud.job.local.RLConfig
 import ai.skymind.skynet.spring.services.ExecutionService
 import ai.skymind.skynet.spring.views.components.ExplainedBlock
 import ai.skymind.skynet.spring.views.layouts.MainLayout
@@ -18,11 +20,16 @@ import com.vaadin.flow.component.html.Span
 import com.vaadin.flow.component.notification.Notification
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.component.textfield.TextArea
 import com.vaadin.flow.component.textfield.TextField
 import com.vaadin.flow.router.BeforeEvent
 import com.vaadin.flow.router.HasUrlParameter
 import com.vaadin.flow.router.Route
+import java.net.URI
+import javax.tools.JavaFileObject
+import javax.tools.SimpleJavaFileObject
 import kotlin.properties.Delegates
+
 
 @Route(value = "model/mdp/edit", layout = MainLayout::class)
 class EditMdpView(
@@ -94,7 +101,7 @@ class EditMdpView(
         })
 
         val rewardDescription = "This is where you enter the code for your reward function. You have the following variables available: agent (your Main Agent), before and after. The variables before and after are the result of calling your getObservation function before and after your doAction function was called. You have to assign the reward to the reward variable. \n\nFor example: \n\n reward = before[0] - after[0];"
-        add(createEditor("Reward Function", rewardDescription, rewardEditor){e, mdp -> mdp.reward = e.value })
+        add(createEditor("Reward Function", rewardDescription, rewardEditor, RLConfig.CompileErrors::reward) { e, mdp -> mdp.reward = e.value })
 
         add(H2("Advanced Options"))
         add(FormLayout().apply {
@@ -109,19 +116,25 @@ class EditMdpView(
 
 
         val importsDescription = "If you need any additional classes in the functions you define here, you can import them here"
-        add(createEditor("Imports", importsDescription, importsEditor){e, mdp -> mdp.imports = e.value })
+        add(createEditor("Imports", importsDescription, importsEditor, RLConfig.CompileErrors::imports) { e, mdp -> mdp.imports = e.value })
 
         val variablesDescription = "If you need additional variables that are going to be available in all of the functions defined here, you can add them in this field."
-        add(createEditor("Class Variables", variablesDescription, variablesEditor){e, mdp -> mdp.variables = e.value })
+        add(createEditor("Class Variables", variablesDescription, variablesEditor, RLConfig.CompileErrors::variables) { e, mdp -> mdp.variables = e.value })
 
         val resetDescription = "If you need to do any additional setup before the simulation can be used, you can do it with this function. You have to following variables available: agent (your Main Agent)."
-        add(createEditor("Reset Function", resetDescription, resetEditor){e, mdp -> mdp.reset = e.value })
+        add(createEditor("Reset Function", resetDescription, resetEditor, RLConfig.CompileErrors::reset) { e, mdp -> mdp.reset = e.value })
 
         val metricsDescription = "If you want to collect any additional metrics during the training, you can do so with this function. You have to following variables available: agent (your Main Agent)."
-        add(createEditor("Metrics Function", metricsDescription, metricsEditor){e, mdp -> mdp.metrics = e.value })
+        add(createEditor("Metrics Function", metricsDescription, metricsEditor, RLConfig.CompileErrors::metrics) { e, mdp -> mdp.metrics = e.value })
     }
 
-    private fun createEditor(title: String, explanation: String, editor: JuicyAceEditor, onChange: (e: HasValue.ValueChangeEvent<String>, mdp: MdpRecord) -> Unit): Component {
+    private fun createEditor(title: String, explanation: String, editor: JuicyAceEditor, errorProperty: (RLConfig.CompileErrors) -> MutableList<String>, onChange: (e: HasValue.ValueChangeEvent<String>, mdp: MdpRecord) -> Unit): Component {
+        val compileOutput = TextArea().apply {
+            isReadOnly = true
+            setWidthFull()
+            isVisible = false
+        }
+
         return VerticalLayout(
                 H3(title),
                 ExplainedBlock(
@@ -133,12 +146,24 @@ class EditMdpView(
                             setWidthFull()
                             height = "10em"
                             addValueChangeListener { e ->
-                                mdp?.let{ onChange(e, it) }
+                                mdp?.let {
+                                    onChange(e, it)
+                                    val allErrors = RLConfig("output", Environment(emptyList()), userSession.model(it.modelId)!!,it).compileErrors()
+                                    val errors = errorProperty(allErrors)
+                                    if(errors.size > 0){
+                                        compileOutput.value = errors.joinToString("\n")
+                                        compileOutput.isVisible = true
+                                    }else{
+                                        compileOutput.value = ""
+                                        compileOutput.isVisible  = false
+                                    }
+                                }
                             }
                         }
                 ).apply{
                     setWidthFull()
-                }
+                },
+                compileOutput
         )
     }
 
@@ -148,4 +173,10 @@ class EditMdpView(
     }
 
 
+}
+
+internal class CharSequenceJavaFileObject(className: String, val content: CharSequence) : SimpleJavaFileObject(URI.create("string:///" + className.replace('.', '/') + JavaFileObject.Kind.SOURCE.extension), JavaFileObject.Kind.SOURCE) {
+    override fun getCharContent(ignoreEncodingErrors: Boolean): CharSequence {
+        return content
+    }
 }
