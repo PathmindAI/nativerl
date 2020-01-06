1608094f7ef5711c9412c185b3c0590f6a9f5fb0
ejunprung <ejunprung@users.noreply.github.com>
2020-01-06 15:29:47 -0800

    Move `batch_mode` and `vf_clip_param` before `sample_batch_size`

--- nativerl/src/main/java/ai/skymind/nativerl/RLlibHelper.java
+++ nativerl/src/main/java/ai/skymind/nativerl/RLlibHelper.java
@@ -534,9 +534,9 @@ public class RLlibHelper {
                             : "        # no learning rate\n")
             + "        'train_batch_size': ray.tune.grid_search(" + Arrays.toString(trainBatchSizes) + "),\n"
             + "        'model': model,\n"
-            + "        'sample_batch_size': " + sampleBatchSize + "," + customParameters + "\n"
             + "        'batch_mode': 'complete_episodes',\n"
-            + "        'vf_clip_param': numpy.inf\n"
+            + "        'vf_clip_param': numpy.inf,\n"            
+            + "        'sample_batch_size': " + sampleBatchSize + "," + customParameters + "\n"
             + "    },\n"
             + (outputDir != null ? "    local_dir='" + outputDir.getAbsolutePath() + "',\n" : "")
             + (checkpoint != null ? "    restore='" + checkpoint.getAbsolutePath() + "',\n" : "")
