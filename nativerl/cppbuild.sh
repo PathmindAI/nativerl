4076da4442ff6fbe41bad1c1947253334c7108c0
Max Pumperla <max.pumperla@googlemail.com>
2020-05-07 15:53:57 +0200

    docs for build script

--- nativerl/cppbuild.sh
+++ nativerl/cppbuild.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# Script to build the Python wrappers from nativerl.cpp with pybind11
 set -eu
 
+# Indicate where to find the JNI library produced by JavaCPP, as required by the Python wrapper
 TARGETDIR="target"
 PLATFORMDIR="$TARGETDIR/classes/ai/skymind/nativerl/$PLATFORM"
-
 CXXSOURCES="$PLATFORMDIR/../nativerl.cpp"
 CXXFLAGS="-O3 -Wall -shared -std=c++11 -fPIC -L$PLATFORMDIR/ -Wl,-rpath,$PLATFORMDIR/ -ljniNativeRL"
 
+# Pick up the include and library directories provided by JavaCPP
 PREVIFS="$IFS"
 IFS="$PLATFORM_PATH_SEPARATOR"
 for P in $PLATFORM_INCLUDEPATH; do
@@ -17,5 +19,6 @@ for P in $PLATFORM_LINKPATH; do
 done
 IFS="$PREVIFS"
 
+# Compile the Python wrappers against the JNI library
 cp $PLATFORMDIR/* $TARGETDIR/
 g++ $CXXFLAGS `python3 -m pybind11 --includes` $CXXSOURCES -o $TARGETDIR/nativerl`python3-config --extension-suffix` '-Wl,-rpath,$ORIGIN/'
