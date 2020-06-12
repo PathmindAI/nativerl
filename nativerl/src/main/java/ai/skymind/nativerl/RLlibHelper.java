efa4cd206d8c240797aacd277c37410c95900067
Alex <alex@malex.io>
2020-06-12 18:57:08 +0800

    Use handlebar to generate rllibtrain.py

--- nativerl/src/main/java/ai/skymind/nativerl/RLlibHelper.java
+++ nativerl/src/main/java/ai/skymind/nativerl/RLlibHelper.java
@@ -3,12 +3,18 @@ package ai.skymind.nativerl;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.helper.ConditionalHelpers;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+import lombok.Getter;
 import org.bytedeco.cpython.*;
 import org.bytedeco.javacpp.*;
-import org.bytedeco.javacpp.indexer.*;
 import org.bytedeco.numpy.*;
 import static org.bytedeco.cpython.global.python.*;
 import static org.bytedeco.numpy.global.numpy.*;
@@ -35,6 +41,7 @@ import static org.bytedeco.numpy.global.numpy.*;
  *   * Works only with continuous actions (doesn't work with discrete ones)
  *   ** Requires PyTorch (doesn't work with TensorFlow)
  */
+@Getter
 public class RLlibHelper {
 
     public static class PythonPolicyHelper implements PolicyHelper {
@@ -423,6 +430,10 @@ public class RLlibHelper {
         return s;
     }
 
+    public String getHiddenLayers() {
+        return hiddenLayers();
+    }
+
     public PolicyHelper createPythonPolicyHelper() throws IOException {
         return new PythonPolicyHelper(rllibpaths, algorithm, checkpoint, environment);
     }
@@ -435,248 +446,21 @@ public class RLlibHelper {
         Files.write(file.toPath(), generatePythonTrainer().getBytes());
     }
 
-    public String generatePythonTrainer() {
+    public String generatePythonTrainer() throws IOException {
         if (environment == null) {
             throw new IllegalStateException("Environment is null.");
         }
-        String trainer = "import glob, gym, nativerl, ray, sys, os, random\n"
-            + "import numpy as np\n"
-            + "from ray.rllib.env import MultiAgentEnv\n"
-            + "from ray.rllib.agents.registry import get_agent_class\n"
-            + "from ray.rllib.utils import seed\n"
-            + "from ray.tune import run, sample_from\n"
-            + "from ray.tune.schedulers import PopulationBasedTraining\n"
-            + "\n"
-            + "jardir = os.getcwd()\n"
-            + "\n"
-            + "class " + environment.getClass().getSimpleName() + "(" + (multiAgent ? "MultiAgentEnv" : "gym.Env") + "):\n"
-            + "    def __init__(self, env_config):\n"
-            + "        # Put all JAR files found here in the class path\n"
-            + "        jars = glob.glob(jardir + '/**/*.jar', recursive=True)\n"
-            + "        nativerl.init(['-Djava.class.path=' + os.pathsep.join(jars + [jardir])]);\n"
-            + "\n"
-            + "        self.nativeEnv = nativerl.createEnvironment('" + environment.getClass().getName() + "')\n"
-            + "        actionSpace = self.nativeEnv.getActionSpace()\n"
-            + "        observationSpace = self.nativeEnv.getObservationSpace()\n"
-            + "        self.action_space = gym.spaces.Discrete(actionSpace.n)\n"
-            + "        self.observation_space = gym.spaces.Box(observationSpace.low[0], observationSpace.high[0], np.array(observationSpace.shape), dtype=np.float32)\n"
-            + "        self.id = '" + environment.getClass().getSimpleName() + "'\n"
-            + "        self.max_episode_steps = 200000\n"
-            + (multiAgent ? "" : "        self.unwrapped.spec = self\n")
-            + "    def reset(self):\n"
-            + "        self.nativeEnv.reset()\n"
-            + (multiAgent
-                ? "        obs = np.array(self.nativeEnv.getObservation())\n"
-                + "        obsdict = {}\n"
-                + "        for i in range(0, obs.shape[0]):\n"
-                + "            obsdict[str(i)] = obs[i]\n"
-                + "        return obsdict\n"
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setSuffix(".hbs");
+        Handlebars handlebars = new Handlebars(loader);
 
-                : "        return np.array(self.nativeEnv.getObservation())\n")
-            + "    def step(self, action):\n"
-            + (multiAgent
-                ? "        actionarray = np.ndarray(shape=(len(action), 1), dtype=np.float32)\n"
-                + "        for i in range(0, actionarray.shape[0]):\n"
-                + "            actionarray[i,:] = action[str(i)].astype(np.float32)\n"
-                + "        reward = np.array(self.nativeEnv.step(nativerl.Array(actionarray)))\n"
-                + "        obs = np.array(self.nativeEnv.getObservation())\n"
-                + "        obsdict = {}\n"
-                + "        rewarddict = {}\n"
-                + "        for i in range(0, obs.shape[0]):\n"
-                + "            obsdict[str(i)] = obs[i]\n"
-                + "            rewarddict[str(i)] = reward[i]\n"
-                + "        return obsdict, rewarddict, {'__all__' : self.nativeEnv.isDone()}, {}\n"
+        handlebars.registerHelpers(ConditionalHelpers.class);
+        handlebars.registerHelper("className", (context, options) -> context.getClass().getName());
+        handlebars.registerHelper("classSimpleName", (context, options) -> context.getClass().getSimpleName());
 
-                : "        reward = self.nativeEnv.step(action)\n"
-                + "        f = open('reward'+self.nativeEnv.getObservation().__str__()+'.txt', 'a')\n"
-                + "        f.write(str(reward)+'\\n')\n"
-                + "        f.close()"
-                + "        return np.array(self.nativeEnv.getObservation()), reward, self.nativeEnv.isDone(), {}\n")
-                + "\n"
-                + "class Stopper:\n"
-                + "    def __init__(self):\n"
-                + "        # Core criteria\n"
-                + "        self.should_stop = False # Stop criteria met\n"
-                + "        self.too_many_iter = False # Max iterations\n"
-                + "        self.too_much_time = False # Max training time\n"
-                + "        self.too_many_episodes = False # Max total episodes\n"
-                + "\n"
-                + "        # Stopping criteria at early check\n"
-                + "        self.no_discovery_risk = False # Value loss never changes\n"
-                + "        self.no_converge_risk = False # Entropy never drops\n"
-                + "\n"
-                + "        # Convergence signals at each iteration from converge check onward\n"
-                + "        self.episode_reward_converged = False # Reward mean changes very little\n"
-                + "        self.value_pred_converged = False # Explained variance changes very little\n"
-                + "\n"
-                + "        # Episode reward behaviour\n"
-                + "        self.episode_reward_window = []\n"
-                + "        self.episode_reward_range = 0\n"
-                + "        self.episode_reward_mean = 0\n"
-                + "        self.episode_reward_mean_latest = 0\n"
-                + "\n"
-                + "        # Entropy behaviour\n"
-                + "        self.entropy_start = 0\n"
-                + "        self.entropy_now = 0\n"
-                + "        self.entropy_slope = 0\n"
-                + "\n"
-                + "        # Value loss behaviour\n"
-                + "        self.vf_loss_window = []\n"
-                + "        self.vf_loss_range = 0\n"
-                + "        self.vf_pred_window = []\n"
-                + "        self.vf_pred_mean = 0\n"
-                + "        self.vf_pred_mean_latest = 0\n"
-                + "\n"
-                + "        # Configs\n"
-                + "        self.episode_reward_range_threshold = " + episodeRewardRangeTh + " # Remove with 0\n"
-                + "        self.entropy_slope_threshold = " + entropySlopeTh + " # Remove with 1\n"
-                + "        self.vf_loss_range_threshold = " + vfLossRangeTh + " # Remove with 0\n"
-                + "        self.value_pred_threshold = " + valuePredTh + " # Remove with 0\n"
-                + "\n"
-                + "    def stop(self, trial_id, result):\n"
-                + "        # Core Criteria\n"
-                + "        self.too_many_iter = result['training_iteration'] >= " + maxIterations + "\n"
-                + (maxTimeInSec > 0
-                ? "        self.too_much_time = result['time_total_s'] >= " + maxTimeInSec + "\n"
-                : "")
-                + "        self.too_many_episodes = result['episodes_total'] >= 30000\n"
-                + "\n"
-                + "        if not self.should_stop and (self.too_many_iter or self.too_much_time or self.too_many_episodes):\n"
-                + "            self.should_stop = True\n"
-                + "            return self.should_stop\n"
-                + "\n"
-                + "        # Collecting metrics for stopping criteria\n"
-                + "        if result['training_iteration'] == 1:\n"
-                + "            self.entropy_start = result['info/learner/default_policy/entropy']\n"
-                + "\n"
-                + "        if result['training_iteration'] <= 50:\n"
-                + "            self.vf_loss_window.append(result['info/learner/default_policy/vf_loss'])\n"
-                + "\n"
-                + "        self.episode_reward_window.append(result['episode_reward_mean'])\n"
-                + "        self.vf_pred_window.append(result['info/learner/default_policy/vf_explained_var'])\n"
-                + "\n"
-                + "        # Experimental Criteria\n"
-                + "\n"
-                + "        # Early stopping filter\n"
-                + "        if result['training_iteration'] == 50:\n"
-                + "            self.entropy_now = result['info/learner/default_policy/entropy']\n"
-                + "            self.entropy_slope = self.entropy_now - self.entropy_start\n"
-                + "            self.vf_loss_range = np.max(np.array(self.vf_loss_window)) - np.min(np.array(self.vf_loss_window))\n"
-                + "            if self.entropy_slope > np.abs(self.entropy_start * self.entropy_slope_threshold):\n"
-                + "                self.no_converge_risk = True\n"
-                + "            if np.abs(self.vf_loss_range) < np.abs(self.vf_loss_window[0] * self.vf_loss_range_threshold):\n"
-                + "                self.no_discovery_risk = True\n"
-                + "\n"
-                + "            # Early stopping decision\n"
-                + "            if not self.should_stop and (self.no_converge_risk or self.no_discovery_risk):\n"
-                + "                self.should_stop = True\n"
-                + "                return self.should_stop\n"
-                + "\n"
-                + "        # Convergence Filter\n"
-                + "        if result['training_iteration'] >= 125:\n"
-                + "            # Episode reward range activity\n"
-                + "            self.episode_reward_range = np.max(np.array(self.episode_reward_window[-50:])) - np.min(np.array(self.episode_reward_window[-50:]))\n"
-                + "            # Episode reward mean activity\n"
-                + "            self.episode_reward_mean = np.mean(np.array(self.episode_reward_window[-75:]))\n"
-                + "            self.episode_reward_mean_latest = np.mean(np.array(self.episode_reward_window[-15:]))\n"
-                + "            # Value function activity\n"
-                + "            self.vf_pred_mean = np.mean(np.array(self.vf_pred_window[-25:]))\n"
-                + "            self.vf_pred_mean_latest = np.mean(np.array(self.vf_pred_window[-5:]))\n"
-                + "\n"
-                + "            # Episode reward leveled off\n"
-                + "            if (np.abs(self.episode_reward_mean_latest - self.episode_reward_mean) / np.abs(self.episode_reward_mean) < self.episode_reward_range_threshold) and (np.abs(self.episode_reward_range) < np.abs(np.mean(np.array(self.episode_reward_window[-50:])) * 2)):\n"
-                + "                self.episode_reward_converged = True\n"
-                + "\n"
-                + "            # Explained variance leveled off\n"
-                + "            if (np.abs(self.vf_pred_mean_latest - self.vf_pred_mean) / np.abs(self.vf_pred_mean) < self.value_pred_threshold):\n"
-                + "                self.value_pred_converged = True\n"
-                + "\n"
-                + "            # Convergence stopping decision\n"
-                + "            if not self.should_stop and self.episode_reward_converged and self.value_pred_converged:\n"
-                + "                self.should_stop = True\n"
-                + "                return self.should_stop\n"
-                + "\n"
-                + "        # Returns False by default until stopping decision made\n"
-                + "        return self.should_stop\n"
-                + "\n"
-                + "stopper = Stopper()\n"
-                + "\n"
-                + "pbt_scheduler = PopulationBasedTraining(\n"
-                + "    time_attr = 'training_iteration',\n"
-                + "    metric = 'episode_reward_mean',\n"
-                + "    mode = 'max',\n"
-                + "    perturbation_interval = 10,\n"
-                + "    quantile_fraction = 0.25,\n"
-                + "    resample_probability = 0.25,\n"
-                + "    log_config = True,\n"
-                + "    hyperparam_mutations = {\n"
-                + "        'lambda': np.linspace(0.9, 1.0, 5).tolist(),\n"
-                + "        'clip_param': np.linspace(0.01, 0.5, 5).tolist(),\n"
-                + "        'entropy_coeff': np.linspace(0, 0.03, 5).tolist(),\n"
-                + "        'lr': [1e-3, 5e-4, 1e-4, 5e-5, 1e-5],\n"
-                + "        'num_sgd_iter': [5, 10, 15, 20, 30],\n"
-                + "        'sgd_minibatch_size': [128, 256, 512, 1024, 2048],\n"
-                + "        'train_batch_size': [4000, 6000, 8000, 10000, 12000]\n"
-                + "    }\n"
-                + ")\n"
-            + "\n"
-            + "# Make sure multiple processes can read the database from AnyLogic\n"
-            + "with open('database/db.properties', 'r+') as f:\n"
-            + "    lines = f.readlines()\n"
-            + "    if 'hsqldb.lock_file=false\\n' not in lines:\n"
-            + "        f.write('hsqldb.lock_file=false\\n')\n"
-            + "\n"
-            + "ray.init(log_to_driver=" + (userLog ? "True" : "False") + ")\n"
-            + "model = ray.rllib.models.MODEL_DEFAULTS.copy()\n"
-            + "model['fcnet_hiddens'] = " + hiddenLayers() + "\n"
-            + "\n"
-            + "trials = run(\n"
-            + "    'PPO',\n"
-            + "    scheduler = pbt_scheduler,\n"
-            + "    num_samples = " + numSamples + ",\n"
-            + "    stop = stopper.stop,\n"
-            + "    config = {\n"
-            + "        'env': " + environment.getClass().getSimpleName() + ",\n"
-            + "        'num_gpus': 0,\n"
-            + "        'num_workers': " + numWorkers + ",\n"
-            + "        'model': model,\n"
-            + "        'use_gae': True,\n"
-            + "        'vf_loss_coeff': 1.0,\n"
-            + "        'vf_clip_param': np.inf,\n"
-            + "        # These params are tuned from a fixed starting value.\n"
-            + "        'lambda': 0.95,\n"
-            + "        'clip_param': 0.2,\n"
-            + "        'lr': 1e-4,\n"
-            + "        'entropy_coeff': 0.0,\n"
-            + "        # These params start off randomly drawn from a set.\n"
-            + "        'num_sgd_iter': sample_from(\n"
-            + "                lambda spec: random.choice([10, 20, 30])),\n"
-            + "        'sgd_minibatch_size': sample_from(\n"
-            + "                lambda spec: random.choice([128, 512, 2048])),\n"
-            + "        'train_batch_size': sample_from(\n"
-            + "                lambda spec: random.choice([4000, 8000, 12000])),\n"
-            + "        # Set rollout samples to episode length\n"
-            + "        'batch_mode': 'complete_episodes',\n"
-            + "        # Auto-normalize observations\n"
-            + "        #'observation_filter': 'MeanStdFilter'\n"
-            + "    },\n"
-            + (outputDir != null ? "    local_dir = '" + outputDir.getAbsolutePath() + "',\n" : "")
-            + "    resume = " + (resume ? "True" : "False") + ",\n"
-            + "    checkpoint_freq = " + checkpointFrequency + ",\n"
-            + "    checkpoint_at_end = True,\n"
-            + "    max_failures = 1,\n"
-            + "    export_formats = ['model'],\n"
-            + "    return_trials = True\n"
-            + ")\n\n"
-            + "errored_trials = []\n"
-            + "for trial in trials:\n"
-            + "    if trial.status != 'TERMINATED':\n"
-            + "        errored_trials += [trial]\n"
-            + "\n"
-            + "if errored_trials:\n"
-            + "    print(errored_trials)\n"
-            + "else:\n"
-            + "    print(\"Training has been completed\")\n";
+        Template template = handlebars.compile("RLlibHelper.py");
+
+        String trainer = template.apply(this);
         return trainer;
     }
 
