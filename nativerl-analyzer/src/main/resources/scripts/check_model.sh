65e50a096bfa5b40b6f23377f980466c7f8ebcaa
kepricon <kepricon@gmail.com>
2020-08-19 11:06:21 -0700

    use PathmindPolicy-1.2.0-SNAPSHOT.jar

--- nativerl-analyzer/src/main/resources/scripts/check_model.sh
+++ nativerl-analyzer/src/main/resources/scripts/check_model.sh
@@ -5,8 +5,7 @@ libDir="/lib"
 cd ${workDir}
 
 export CLASSPATH=$(find ${libDir}/pathmind -iname '*.jar' -printf '%p:')
-export POLICY=$(find ${libDir}/policy -iname "*single.jar" -printf '%p:')
-export CLASSPATH=$PWD:$PWD/model.jar:${CLASSPATH}:${POLICY}:/pathmind-model-analyzer.jar
+export CLASSPATH=$PWD:$PWD/model.jar:${CLASSPATH}:/pathmind-model-analyzer.jar
 export MODEL_PACKAGE=$(unzip -l model.jar | grep Main.class | awk '{print $4}' | xargs dirname)
 export MODEL_PACKAGE_NAME=$(echo ${MODEL_PACKAGE} | sed 's/\//\./g')
 export AGENT_CLASS="$MODEL_PACKAGE_NAME.Main"
