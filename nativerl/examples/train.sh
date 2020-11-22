59fb1f649195d2d5e793a20c3ef76acd444645b6
kepricon <kepricon@gmail.com>
2020-11-22 15:18:05 -0800

    change priority(Simulation first, RLExperiment second)

--- nativerl/examples/train.sh
+++ nativerl/examples/train.sh
@@ -3,11 +3,11 @@ export MODEL_PACKAGE=$(for m in $(ls model.jar lib/model*.jar 2> /dev/null) ; do
 export MODEL_PACKAGE_NAME=$(echo $MODEL_PACKAGE | sed 's/\//\./g')
 export ENVIRONMENT_CLASS="$MODEL_PACKAGE_NAME.PathmindEnvironment"
 
-EXPERIMENT_TYPE="RLExperiment"
-export SIMULATION_PACKAGE=$(for m in $(ls model.jar lib/model*.jar 2> /dev/null) ; do unzip -l $m | grep RLExperiment.class | grep -v pathmind/policyhelper; done | awk '{print $4}' | xargs dirname)
+EXPERIMENT_TYPE="Simulation"
+export SIMULATION_PACKAGE=$(for m in $(ls model.jar lib/model*.jar 2> /dev/null) ; do unzip -l $m | grep Simulation.class | grep -v pathmind/policyhelper; done | awk '{print $4}' | xargs dirname)
 if [[ -z "$SIMULATION_PACKAGE" ]]; then
-    export SIMULATION_PACKAGE=$(for m in $(ls model.jar lib/model*.jar 2> /dev/null) ; do unzip -l $m | grep Simulation.class | grep -v pathmind/policyhelper; done | awk '{print $4}' | xargs dirname)
-    EXPERIMENT_TYPE="Simulation"
+    export SIMULATION_PACKAGE=$(for m in $(ls model.jar lib/model*.jar 2> /dev/null) ; do unzip -l $m | grep RLExperiment.class | grep -v pathmind/policyhelper; done | awk '{print $4}' | xargs dirname)
+    EXPERIMENT_TYPE="RLExperiment"
 fi
 export SIMULATION_PACKAGE_NAME=$(echo $SIMULATION_PACKAGE | sed 's/\//\./g')
 export SIMULATION_CLASS="$SIMULATION_PACKAGE_NAME.$EXPERIMENT_TYPE"
