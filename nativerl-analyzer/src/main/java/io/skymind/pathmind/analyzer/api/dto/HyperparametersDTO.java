5c03bba8b17f4cba80fee353f30bcbd548bc8caf
kepricon <kepricon@gmail.com>
2021-02-17 17:02:45 -0800

    leave not blank annotation for the future

--- nativerl-analyzer/src/main/java/io/skymind/pathmind/analyzer/api/dto/HyperparametersDTO.java
+++ nativerl-analyzer/src/main/java/io/skymind/pathmind/analyzer/api/dto/HyperparametersDTO.java
@@ -45,24 +45,33 @@ public class HyperparametersDTO {
 
     @ApiModelProperty(value = "Number of observations extracted from model", example = "10", required =
             true)
+//    @NotBlank(message = "Number of observations cannot be blank")
     private String observations;
 
     @ApiModelProperty(value = "Observations names extracted from model", example = "[\"orderQueueSize\", \"collectQueueSize\"]", required =
             true)
+//    @NotBlank(message = "Observation names cannot be empty")
     private List<String> observationNames;
 
+//    @NotBlank(message = "Observation types cannot be empty")
     private List<String> observationTypes;
 
     @ApiModelProperty(value = "Number of actions extracted from model", example = "5", required = true)
+//    @NotBlank(message = "Number of actions cannot be blank")
     private String actions;
 
     @ApiModelProperty(value = "Length of reward variables array extracted from model", example = "7", required = true)
+//    @NotBlank(message = "Reward variables count cannot be blank")
     private String rewardVariablesCount;
 
     @ApiModelProperty(value = "Reward variable names extracted from model", example = "[\"var1\", \"var2\"]", required = true)
+//    @NotNull(message = "Reward variable names is required")
+//    @NotEmpty(message = "Reward variable names cannot be empty")
     private List<String> rewardVariableNames;
 
     @ApiModelProperty(value = "Reward variable types extracted from model", example = "[\"int\", \"boolean\"]", required = true)
+//    @NotNull(message = "Reward variable names is required")
+//    @NotEmpty(message = "Reward variable types cannot be empty")
     private List<String> rewardVariableTypes;
 
     @ApiModelProperty(value = "Reward function definition", required =
@@ -75,9 +84,11 @@ public class HyperparametersDTO {
     private String failedSteps;
 
     @ApiModelProperty(value = "the number of agents", required = true)
+//    @NotBlank(message = "Agents cannot be blank")
     private String agents;
 
     @ApiModelProperty(value = "Extraction mode (single/multi)", required = true)
+//    @NotBlank(message = "Mode cannot be blank")
     private String mode;
 
     public static HyperparametersDTO of(@NotEmpty List<String> hyperparametersList) {
