ed7687b8f594ea5a7bb438fb734e87f0c3ae1f79
kepricon <kepricon@gmail.com>
2021-03-26 18:11:39 -0700

    pass loggers to freezing(mc_rollout)

--- nativerl/python/pathmind_training/freezing.py
+++ nativerl/python/pathmind_training/freezing.py
@@ -19,7 +19,7 @@ def find(key, value):
                     yield result
 
 
-def mc_rollout(steps, checkpoint, environment, env_name, callbacks, output_dir, input_config,
+def mc_rollout(steps, checkpoint, environment, env_name, callbacks, loggers, output_dir, input_config,
                step_tolerance=1000000, algorithm='PPO'):
     """
     Monte Carlo rollout. Currently set to return brief summary of rewards and metrics.
@@ -44,6 +44,7 @@ def mc_rollout(steps, checkpoint, environment, env_name, callbacks, output_dir, 
         algorithm,
         num_samples=1,
         stop={'training_iteration': 1},
+        loggers=loggers,
         config=config,
         local_dir=output_dir,
         restore=checkpoint,
@@ -59,7 +60,7 @@ def mc_rollout(steps, checkpoint, environment, env_name, callbacks, output_dir, 
     return mean_reward, range_of_rewards, trials.get_best_logdir(metric="episode_reward_mean", mode="max")
 
 
-def freeze_trained_policy(env, env_name, callbacks, trials, output_dir: str, algorithm: str, is_discrete: bool,
+def freeze_trained_policy(env, env_name, callbacks, trials, loggers, output_dir: str, algorithm: str, is_discrete: bool,
                           filter_tolerance: float = 0.85, mc_steps: int = 10000,
                           step_tolerance: int = 100_000_000):
     """Freeze the trained policy at several temperatures and pick the best ones.
@@ -68,6 +69,7 @@ def freeze_trained_policy(env, env_name, callbacks, trials, output_dir: str, alg
     :param env_name: name of the env (str)
     :param callbacks: ray Callbacks class
     :param trials: The trials returned from a ray tune run.
+    :param loggers: loggers for mc_rollout
     :param output_dir: output directory for logs
     :param algorithm: the Rllib algorithm used (defaults to "PPO")
     :param is_discrete: for continuous actions we currently skip this step.
@@ -99,7 +101,7 @@ def freeze_trained_policy(env, env_name, callbacks, trials, output_dir: str, alg
             config['model'] = {'custom_action_dist': temp}
 
         mean_reward_dict[temp], range_reward_dict[temp], log_dir_dict[temp] = \
-            mc_rollout(mc_steps, checkpoint_path, env, env_name, callbacks, output_dir, config,
+            mc_rollout(mc_steps, checkpoint_path, env, env_name, callbacks, loggers, output_dir, config,
                        step_tolerance, algorithm)
 
     # Filter out policies with under (filter_tolerance*100)% of max mean reward
