7ae6094de4e77fbb3e5dfba33001dfff0ffbd2ad
Slin Lee <slin@pathmind.com>
2021-10-09 09:59:26 -0700

    Build model anaylzer docker using training-static S3 bucket (#418)
    * Build model anaylzer docker using training-static S3 bucket
    
    * Build Model Analyzer Docker image as part of PR tests
    
    * Run docker builds in parallel in Jenkins
    
    * Add folder when downloading MA lib
    
    * Combine RUN commands in MA Dockerfile
    
    * Combine RUN commands in NativeRL dockerfile
    
    * Split out scl command in Dockerfile

--- nativerl-analyzer/src/main/resources/scripts/download_lib.sh
+++ nativerl-analyzer/src/main/resources/scripts/download_lib.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 S3BUCKET=$1
-default_file=$2
-alternative_file=$3
+folder=$2
+default_file=$3
+alternative_file=$4
 
-aws s3api head-object --bucket ${S3BUCKET} --key ${default_file} || not_exist=true
+aws s3api head-object --bucket ${S3BUCKET} --key ${folder}/${default_file} || not_exist=true
 if [ ${not_exist} ]; then
   file=${alternative_file}
 else
   file=${default_file}
 fi
 
-aws s3 cp s3://${S3BUCKET}/${file} ./
+aws s3 cp s3://${S3BUCKET}/${folder}/${file} ./
 ext="${file#*.}"
 if [ ${ext} = "tar.gz" ]; then
     tar -xzf ${file}
