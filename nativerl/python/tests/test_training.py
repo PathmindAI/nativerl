122e0edd235337c1d8b91e9b625206ffa96134cb
Slin Lee <slin@pathmind.com>
2021-10-15 18:03:50 -0700

    Create different folders for the training auto tests
    Otherwise they fail the second time you run them

--- nativerl/python/tests/test_training.py
+++ nativerl/python/tests/test_training.py
@@ -1,27 +1,53 @@
+from random import randint
+
 import pytest
 import run
 
 
 @pytest.mark.integration
 def test_gym_training():
-    run.main(environment="CartPole-v0", is_gym=True, max_episodes=1)
+    output_dir = f"testoutputs/test-gym-training-{randint(0,1000)}"
+    run.main(
+        environment="CartPole-v0", is_gym=True, max_episodes=1, output_dir=output_dir
+    )
 
 
 @pytest.mark.integration
 def test_or_gym_training():
-    run.main(environment="Knapsack-v0", is_gym=True, max_episodes=1)
+    output_dir = f"testoutputs/test-or-gym-training-{randint(0,1000)}"
+    run.main(
+        environment="Knapsack-v0", is_gym=True, max_episodes=1, output_dir=output_dir
+    )
 
 
 @pytest.mark.integration
 def test_freezing():
-    run.main(environment="Knapsack-v0", is_gym=True, max_episodes=1, freezing=True)
+    output_dir = f"testoutputs/test-freezing-{randint(0,1000)}"
+    run.main(
+        environment="Knapsack-v0",
+        is_gym=True,
+        max_episodes=1,
+        freezing=True,
+        output_dir=output_dir,
+    )
 
 
 @pytest.mark.integration
 def test_pathmind_env_module():
-    run.main(environment="tests.cartpole.PathmindEnvironment", max_episodes=1)
+    output_dir = f"testoutputs/test-pathmind-env-module-{randint(0,1000)}"
+    run.main(
+        environment="tests.cartpole.PathmindEnvironment",
+        max_episodes=1,
+        output_dir=output_dir,
+    )
 
 
 @pytest.mark.integration
 def test_gym_module():
-    run.main(environment="tests.gym_cartpole.CartPoleEnv", is_gym=True, max_episodes=1)
+    output_dir = f"testoutputs/test-gym-module-{randint(0,1000)}"
+    run.main(
+        environment="tests.gym_cartpole.CartPoleEnv",
+        is_gym=True,
+        max_episodes=1,
+        output_dir=output_dir,
+    )
