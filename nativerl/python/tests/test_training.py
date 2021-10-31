06a2a0947e0389bc50d70e211be2dc807a1988e8
Slin Lee <slin@pathmind.com>
2021-10-30 23:00:28 -0700

    Add test and example of two reward term simulation

--- nativerl/python/tests/test_training.py
+++ nativerl/python/tests/test_training.py
@@ -46,6 +46,17 @@ def test_pathmind_env_module():
         output_dir=output_dir,
     )
 
+@pytest.mark.integration
+def test_pathmind_sim_module():
+    ray.shutdown()
+    output_dir = f"testoutputs/test-pathmind-sim-module-{randint(0,1000)}"
+    run.main(
+        is_pathmind_simulation=True,
+        environment="tests.mouse.two_reward.TwoRewardMouseAndCheese",
+        max_episodes=1,
+        output_dir=output_dir,
+    )
+
 
 @pytest.mark.integration
 def test_gym_module():
