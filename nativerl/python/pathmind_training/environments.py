b6db95e3ec8f5e027f6f9f57ba977cdf8db27128
Slin Lee <slin@pathmind.com>
2021-11-15 12:06:54 -0800

    Revert `bg_multi_mini` reward balancing changes

--- nativerl/python/pathmind_training/environments.py
+++ nativerl/python/pathmind_training/environments.py
@@ -277,15 +277,9 @@ def get_environment(
                 done_dict["__all__"] = all(done_dict.values())
 
                 if self.use_reward_terms and done_dict["__all__"]:
-                    max_array = np.zeros(len(reward_array), dtype=np.float32)
-                    for values in self.term_contributions_dict.values():
-                        max_array = np.array(
-                            [
-                                max(max_array[i], abs(values[i]))
-                                for i in range(len(reward_array))
-                            ]
-                        )
-                    self.term_contributions = max_array
+                    self.term_contributions += sum(
+                        self.term_contributions_dict.values()
+                    ) / len(self.term_contributions_dict)
                 return obs_dict, reward_dict, done_dict, {}
 
             else:
